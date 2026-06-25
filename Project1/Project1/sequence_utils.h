9691bb843004d306d9651754087c258e6b6e833f
Palxv <20pashapasha18@gmail.com>
2026-06-25 15:52:45 +0300

    Delete Project1 directory

--- Project1/Project1/sequence_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEQUENCE_UTILS_H
-#define SEQUENCE_UTILS_H
-
-bool isNumber(const char* s);
-void sortGoodBad(double* arr, int size);
-void calculateMeans(double* arr, int size, double& outA, double& outH);
-
-#endif
